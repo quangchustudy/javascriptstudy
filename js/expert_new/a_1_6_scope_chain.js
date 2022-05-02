3fc2328b44597e4e24387c7f8a72a56f58493a4f
DESKTOP-B09ANSL\Quang <chuvanquang0104@gmail.com>
2022-05-02 22:19:06 +0700

    commit

--- js/expert_new/a_1_6_scope_chain.js
+++ js/expert_new/a_1_6_scope_chain.js
@@ -1,14 +1,18 @@
+/**
+ * @description: SCOPE CHAIN
+ * @type {string}
+ */
 var color = 'red';
 var name = 'foo';
 
 function first() {
   var color = 'green';
   console.log(color);
-  
+
   function second() {
     console.log(name);
   }
-  
+
   second();
 }
 
